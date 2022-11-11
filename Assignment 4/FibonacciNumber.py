0297a89e970eae82678c44c9b0f3f3027042c545
xhiti <mexhitkurti10@gmail.com>
2022-11-11 21:57:55 +0100

    Fibonacci Number

---
+++ Assignment 4/FibonacciNumber.py
@@ -0,0 +1,27 @@
+# 1. Write a function that calculates and returns the n-th Fibonacci number using for loop:
+
+def fib(n):
+    if n < 0:
+        return -1
+
+    if 0 >= n <= 1:
+        return 1
+
+    previousNumber = 0
+    currentNumber = 1
+
+    for i in range(n - 1):
+        newNumber = previousNumber + currentNumber
+        previousNumber = currentNumber
+        currentNumber = newNumber
+
+    return currentNumber
+
+
+givenInput = str(input("Please enter a number: "))
+
+if givenInput.strip("-").isdigit():
+    fibonacciNumber = fib(int(givenInput))
+    print("fib(" + str(givenInput) + ") = " + str(fibonacciNumber))
+else:
+    print("INVALID '" + str(givenInput) + "' given, input must be an INTEGER!")
