9c33b0573f6e349748d4d02e8a55329b939fb901
xhiti <mexhitkurti10@gmail.com>
2022-11-13 16:52:19 +0100

    Assignment 4

--- Assignment 4/ClippedList.py
+++ Assignment 4/ClippedList.py
@@ -1,30 +1,30 @@
 # 2. Write a function CLIP that returns a list of clipped values based on arbitrary many input values:
-def isValidType(number):
-    return type(number) in (float, int)
-
 
 def clip(*values, min_=0, max_=1):
     clippedList = []
+
     if len(values) == 0:
         return clippedList
-    elif not isValidType(min_):
+
+    if not type(min_) in (float, int):
         print("MIN value must be a number (integer/float)!")
         return clippedList
-    elif not isValidType(max_):
+
+    if not type(max_) in (float, int):
         print("MAX value must be a number (integer/float)!")
         return clippedList
-    else:
-        for value in values:
-            if isValidType(value):
-                if value < min_:
-                    clippedList.append(min_)
-                elif value > max_:
-                    clippedList.append(max_)
-                else:
-                    clippedList.append(value)
+
+    for value in values:
+        if type(value) in (float, int):
+            if value < min_:
+                clippedList.append(min_)
+            elif value > max_:
+                clippedList.append(max_)
             else:
-                print("Value '" + str(value) + "' is not a number (integer/float)!")
-                return []
+                clippedList.append(value)
+        else:
+            print("Value '" + str(value) + "' is not a number (integer/float)!")
+            break
 
     return clippedList
 
